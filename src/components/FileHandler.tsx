975cbd24c734c99b18921e4341fd80d468543ca5
riefer02 <andrew.riefenstahl@gmail.com>
2021-11-24 23:29:16 -0600

    interface prototype test

--- src/components/FileHandler.tsx
+++ src/components/FileHandler.tsx
@@ -3,51 +3,59 @@ import axios from 'axios';
 import { useSelector } from 'react-redux';
 
 export default function FileHandler() {
-    const filesMeta = useSelector((state) => state.file.filesMeta);
-    const [formattedFilesMeta, setFormattedFilesMeta] = useState([]);
-    const [message, setMessage] = useState(`I'm the File Handler Component, thank you for making me real.`)
+  const filesMeta = useSelector((state) => state.file.filesMeta);
+  const [formattedFilesMeta, setFormattedFilesMeta] = useState([]);
+  const [message, setMessage] = useState(
+    `I'm the File Handler Component, thank you for making me real.`
+  );
 
-    const fileReader = async (files) => {
-        for (const file of files) {
-            let blob = await fetch(file.localUrl).then(res => res.blob());
-            var fd = new FormData();
-            fd.append('upload-meta', JSON.stringify(file));
-            fd.append('upload-file', blob, file.name);
-            setFormattedFilesMeta(fd);
-        }
+  const fileReader = async (files) => {
+    for (const file of files) {
+      let blob = await fetch(file.localUrl).then((res) => res.blob());
+      var fd = new FormData();
+      fd.append('upload-meta', JSON.stringify(file));
+      fd.append('upload-file', blob, file.name);
+      setFormattedFilesMeta(fd);
+    }
+  };
+
+  const callAPI = async () => {
+    interface Response {
+      message: string;
     }
 
-    const callAPI = async () => {
-        const response = await axios.post(
-            `${import.meta.env.VITE_API_URL}audio/upload`,
-            formattedFilesMeta,
-            {
-                headers: { 'Content-Type': 'application/json' }
-            }
-        )
-            .then(res => res.data)
-            .catch(err => err);
-        setMessage(response.message)
-    };
+    const response: object = await axios
+      .post(`${import.meta.env.VITE_API_URL}audio/upload`, formattedFilesMeta, {
+        headers: { 'Content-Type': 'application/json' },
+      })
+      .then((res) => res.data)
+      .catch((err) => err);
+    setMessage(response.message);
+  };
 
-    const handleSubmit = () => {
-        // Change to forEach file callAPI()
-        filesMeta.length > 0 ? callAPI() : setMessage(`There's nothing to send.`)
-    }
+  const handleSubmit = () => {
+    // Change to forEach file callAPI()
+    filesMeta.length > 0 ? callAPI() : setMessage(`There's nothing to send.`);
+  };
 
-    useEffect(() => {
-        filesMeta.length > 0 ? setMessage(`I've got some data for you.`) : null
-        fileReader(filesMeta)
-    }, [filesMeta])
+  useEffect(() => {
+    filesMeta.length > 0 ? setMessage(`I've got some data for you.`) : null;
+    fileReader(filesMeta);
+  }, [filesMeta]);
 
-    useEffect(() => {
-        console.log('Formatted Files Meta Data', formattedFilesMeta);
-    }, [formattedFilesMeta])
+  useEffect(() => {
+    console.log('Formatted Files Meta Data', formattedFilesMeta);
+  }, [formattedFilesMeta]);
 
-    return (
-        <div className="file-handler">
-            <div className="file-handler__message">{message}</div>
-            <button className="file-handler__submit-btn" onClick={() => handleSubmit()}>Submit</button>
-        </div>
-    )
+  return (
+    <div className="file-handler">
+      <div className="file-handler__message">{message}</div>
+      <button
+        className="file-handler__submit-btn"
+        onClick={() => handleSubmit()}
+      >
+        Submit
+      </button>
+    </div>
+  );
 }
